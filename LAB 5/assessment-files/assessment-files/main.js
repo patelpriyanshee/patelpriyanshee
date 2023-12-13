fa26404ebdf1a470f845e36c64ff96fa951cf011
patelpriyanshee <148272017+patelpriyanshee@users.noreply.github.com>
2023-12-12 22:15:41 -0500

    commit-3
    final change

--- LAB 5/assessment-files/assessment-files/main.js
+++ LAB 5/assessment-files/assessment-files/main.js
@@ -16,6 +16,13 @@ showHideBtn.onclick = function() {
   }
 };
 
+/* added */
+showHideBtn.onkeydown = (e) => {
+  if (e.key === "Enter") {
+    showHideBtn.click();
+  }
+}
+
 // functionality for adding a new comment via the comments form
 
 const form = document.querySelector('.comment-form');
@@ -45,3 +52,42 @@ function submitComment() {
   nameField.value = '';
   commentField.value = '';
 }
+
+// audio transcript
+const transcript = document.querySelector('.transcript');
+const transcriptBtn = document.querySelector('.transcript-button');
+const transcriptCon = document.querySelector('.transcript-container')
+
+transcriptBtn.onclick = () => {
+  if(transcriptBtn.textContent === 'Show transcript') {
+    transcript.style.display = "block";
+    transcriptBtn.textContent = 'Hide transcript';
+  } else {
+    transcript.style.display = "none";
+    transcriptBtn.textContent = 'Show transcript'
+  }
+}
+
+// hamburger menu
+const nav = document.querySelector("nav")
+const hamIcon = document.querySelector(".hamIcon")
+const navMenu = document.querySelector("nav ul")
+navMenu.className = "hidden";
+
+hamIcon.onclick = () => {
+  if(navMenu.className === "hidden") {
+    navMenu.style.visibility = "visible";
+    navMenu.className = "visible";
+  } else if (navMenu.className === "visible") {
+    navMenu.style.visibility = "hidden";
+    navMenu.className = "hidden";
+  }
+}
+
+/* How can I hide navMenu when any of the link is pressed?
+failed attempt:
+nav.addEventListener("focusout", ()=> {
+  navMenu.style.visibility = "hidden";
+  navMenu.className = "hidden";
+})
+*/
